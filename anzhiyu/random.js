5ab6d1559ee5faf29848d663df896e7311a01e4c
reqlong <yuqilong576@gmail.com>
2024-05-20 16:32:31 +0800

    Site updated: 2024-05-20 16:32:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/20/hello-world/","2024/05/20/iperf打流使用方法/"];function toRandomPost(){
+var posts=["2024/05/20/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
