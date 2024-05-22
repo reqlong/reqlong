c95959456e806aa595dc233a41cbc1a9602e1409
reqlong <yuqilong576@gmail.com>
2024-05-22 09:34:03 +0800

    Site updated: 2024-05-22 09:34:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/20/cs/","2024/05/22/测试文章/","2024/05/22/部署Harbor私有镜像仓库/","2024/05/20/hello-world/"];function toRandomPost(){
+var posts=["2024/05/20/hello-world/","2024/05/22/部署Harbor私有镜像仓库/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
