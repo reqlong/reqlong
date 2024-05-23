98af744c0441f23c68500e536b7191a580c6fefd
reqlong <yuqilong576@gmail.com>
2024-05-23 13:54:40 +0800

    Site updated: 2024-05-23 13:54:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/uncategorized/14743/","2024/uncategorized/16107/","2024/uncategorized/44097/","2024/uncategorized/37773/"];function toRandomPost(){
+var posts=["posts/37773/","posts/16107/","posts/14743/","posts/44097/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
